be01344ec45860f60108dfe9f3027c9871197690
GitHub <david.novogrodsky@gmail.com>
2019-04-25 21:21:51 -0500

    More experiments: String Interpolation and Property Binding.

--- my-first-angle/src/app/server/server.component.ts
+++ my-first-angle/src/app/server/server.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
+/*
+String Interpolation returning a string or something that cas be cast as
+a string.
+ */
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- my-first-angle/src/app/servers/servers.component.ts
+++ my-first-angle/src/app/servers/servers.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+/* This contains the business logic
+*  can use String Interpolation or Property Binding to send to html */
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  /* This is Property Binding */
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
