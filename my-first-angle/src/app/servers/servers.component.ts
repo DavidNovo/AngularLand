7065dcc1e690ca9e8a7fed43dd4761867c355494
u001djn <david.novogrodsky@carlsonwagonlit.com>
2019-04-29 08:25:56 -0500

    Event Binding
    This is an example of event binding.

--- my-first-angle/src/app/servers/servers.component.ts
+++ my-first-angle/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   /* This is Property Binding */
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';  // This will be used for Event Binding.
+  serverName = '';  // used to caputre info from html
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,12 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server Created.';
+  }
+
+  onUpdateServerName(event: any) {
+    /* console.log(event);*/
+    this.serverName = (<HTMLInputElement>event.target).value;
+
   }
 
 
