cd3193c8bcac4d4642e3f30d8b44b2a4fcdbf5fa
GitHub <david.novogrodsky@gmail.com>
2019-05-06 21:56:37 -0500

    AAssignment three, added button to toggle display of a paragraph

--- my-first-angle/src/app/assignment-three/assignment-three.component.ts
+++ my-first-angle/src/app/assignment-three/assignment-three.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-three.component.css']
 })
 export class AssignmentThreeComponent implements OnInit {
-
+  showPara: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
+
+  toggleDisplay(): void {
+    if (this.showPara) {
+      this.showPara = false;
+    } else {
+      this.showPara = true;
+    }
+  }
+
 
 }
